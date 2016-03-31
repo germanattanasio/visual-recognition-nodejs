409524c128a7f930c820dbdd80a1d14fcab4804b
JAMES ZHANG <jzhang300@us.ibm.com>
2016-03-31 14:00:54 -0500

    update test

--- test/test.express.js
+++ test/test.express.js
@@ -43,39 +43,45 @@ describe('express', function() {
 
   });
 
+  // try 50 images each
+
+  // try lower than 50
+
+  // try 500 images?
+
   // api/classifiers
   it('200 when calling /api/classifiers', function(done) {
 
   });
 
   // 200 when calling
-  // it('200 when calling classify', function(done) {
-  //   var server = 'https://gateway.watsonplatform.net:443',
-  //     classifier_id = '<classifier-id>',
-  //     path = '/api/classify',
-  //     text = 'classify me';
-  //
-  //   var response = {
-  //     classifier_id: classifier_id,
-  //     text: text,
-  //     top_class: 'bar',
-  //     classes: [{
-  //       class_name: 'bar',
-  //       confidence: 0.99
-  //     }, {
-  //       class_name: 'foo',
-  //       confidence: 0.01
-  //     }]
-  //   };
-  //   nock(server)
-  //     .post('/natural-language-classifier/api/v1/classifiers/%3Cclassifier-id%3E/classify',
-  //       { text: text})
-  //     .reply(200, response);
-  //
-  //   request(app)
-  //     .post(path)
-  //     .send({text: text})
-  //     .expect(200, response, done);
-  // });
+  it('200 when calling classify', function(done) {
+    var server = 'https://gateway.watsonplatform.net:443',
+      classifier_id = '<classifier-id>',
+      path = '/api/classify',
+      text = 'classify me';
+
+    var response = {
+      classifier_id: classifier_id,
+      text: text,
+      top_class: 'bar',
+      classes: [{
+        class_name: 'bar',
+        confidence: 0.99
+      }, {
+        class_name: 'foo',
+        confidence: 0.01
+      }]
+    };
+    nock(server)
+      .post('/natural-language-classifier/api/v1/classifiers/%3Cclassifier-id%3E/classify',
+        { text: text})
+      .reply(200, response);
+
+    request(app)
+      .post(path)
+      .send({text: text})
+      .expect(200, response, done);
+  });
 
 });
