a7f9502474b422c2af73e7665fc83918a13dcd76
Christopher Falone <ccfalone@us.ibm.com>
2016-06-02 15:51:02 -0500

    wrote some e2e/integration tests

--- test/test.thermometer.js
+++ test/test.thermometer.js
@@ -4,9 +4,10 @@ casper.start('http://localhost:3000/thermometer', function(result) {
 
 casper.thenOpen('http://localhost:3000/thermometer?score=0.5', function(result) {
   casper.test.assert(result.status == 200, 'valid score gets a 200');
+  //casper.test.assert(body.score == 0.5, 'score is returned');
 });
 
-casper.thenOpen('http://localhost:3000/thermometer?score=-1', function(result, body) {
+casper.thenOpen('http://localhost:3000/thermometer?score=-1', function(result) {
   casper.test.assert(result.status == 400, 'invalid score gets a 400');
   var body = JSON.parse(this.getPageContent());
   casper.test.assert(body.error == 'Score value invalid', 'error message is correct');
