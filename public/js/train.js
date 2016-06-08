6d1fd128194a214c9da054b7589aa376d4754195
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2016-06-08 14:28:54 -0400

    eslint issues fixed

--- public/js/train.js
+++ public/js/train.js
@@ -24,8 +24,7 @@ var currentPage = require('./demo.js').currentPage;
 
 $(document).ready(function() {
   $('._training--example').click(function() {
-    var current_example = $(this);
-
+    var currentExample = $(this);
 
     $('.showing div._examples--class__selected button').click();
     var kind = $(this).data('kind');
@@ -34,14 +33,14 @@ $(document).ready(function() {
       $('.showing').removeClass('showing');
       $('._container--bundle-form').removeClass('active');
       $('._training--example').map(function(index, item) {
-        $(item).css('opacity','1.0');
+        $(item).css('opacity', '1.0');
       });
     } else {
       $('._training--example').map(function(index, item) {
-        if (!$(item).is(current_example)) {
-          $(item).css('opacity','0.5');
+        if (!$(item).is(currentExample)) {
+          $(item).css('opacity', '0.5');
         } else {
-          $(item).css('opacity','1.0');
+          $(item).css('opacity', '1.0');
         }
       });
       $('.showing').removeClass('showing');
