73ad9123ffa4964a430f02e3fdb51c51b4bc237f
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-08-17 16:31:41 -0400

    confirm message for retraining

--- public/js/train.js
+++ public/js/train.js
@@ -113,6 +113,12 @@ $(document).ready(function() {
   }
 
   $('button[type=reset]').click(function() {
+    if ($(event.target).hasClass('reset--classifier')) {
+      if (!confirm("Are you sure you want to delete the classifier?")) {
+        $(event.target).blur();
+        return;
+      }
+    }
     if ($('.showing div._examples--class__selected button').length > 0) {
       $('.showing div._examples--class__selected button').click();
     } else {
