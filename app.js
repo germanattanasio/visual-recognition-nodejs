67e533c0425c73a2c75013a8fdb03e2c01327a3e
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-10-17 12:48:54 -0400

    [ci skip] update jsdoc

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var TWENTY_SECONDS = 20000;
 require('./config/express')(app);
 
 // Create the service wrapper
-// If no API Key is provided here, the watson-developer-cloud@2.x.x library will check for an ALCHEMY_LANGUAGE_API_KEY 
+// If no API Key is provided here, the watson-developer-cloud@2.x.x library will check for an VISUAL_RECOGNITION_API_KEY 
 // environment property and then fall back to the VCAP_SERVICES property provided by Bluemix.
 var visualRecognition = new watson.VisualRecognitionV3({
   // api_key: '<api-key>',
